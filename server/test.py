9bbc6832ea57cb0e160f68bd01265ae8f38ed2ee
Onesco <Okonkwoonesmus5@gmail.com>
2022-06-23 18:33:04 +0100

    refactored the code

--- server/test.py
+++ server/test.py
@@ -1,40 +1,16 @@
-from socket import socket, AF_INET, SOCK_STREAM
-from threading import Thread
 import time
-# Global CONSTANTS
-HOST = "localhost"
-PORT = 3200
-ADDR = (HOST, PORT)
-BUFSIZE = 512
+from connect_client import Client
 
-# Global VARIABLE
-messages = []
-# create server
-CLIENT_SOCKET= socket(AF_INET, SOCK_STREAM)
-CLIENT_SOCKET.connect(ADDR)
-
-def send_message(msg):
-    CLIENT_SOCKET.send(bytes(msg, 'utf8'))
-    if msg == '{quit}':
-        CLIENT_SOCKET.close()
-    return None
-
-
-def receive_message():
-    while True:
-        try:
-            msg = CLIENT_SOCKET.recv(BUFSIZE).decode()
-            messages.append(msg)
-            print(msg)
-        except Exception as error:
-            print('[EXCEPTION]', error)
-            break
-
-receive_thread = Thread(target=receive_message)
-receive_thread.start()
-
-send_message("onesmus")
+c1 = Client('Onesmus')
 time.sleep(0.1)
-send_message("hello world")
+c1.send_message("good evening guys")
 time.sleep(0.1)
-send_message("how far")
+c1.send_message("how far")
+time.sleep(2)
+# c1.send_message("{quit}")
+c2 = Client('Santos')
+time.sleep(0.1)
+c2.send_message("good evening guys")
+time.sleep(0.1)
+c2.send_message("what is up")
+time.sleep(2)
